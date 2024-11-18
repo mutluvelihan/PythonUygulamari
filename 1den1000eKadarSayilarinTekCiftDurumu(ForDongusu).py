a27ee9fca941ed0b0b8043cb2d21fa2db8c0d73f
Velihan MUTLU <velihanmutlu1@gmail.com>
2024-11-18 21:45:21 +0300

    1 den 1000 e kadar sayilarin teklik ciftlik durumu hesaplandi ve girilen sayi kadar ekrana yazdirma islemi yapidi

---
+++ 1den1000eKadarSayilarinTekCiftDurumu(ForDongusu).py
@@ -0,0 +1,6 @@
+sayi=range(1,1001)
+for i in sayi:
+    if i%2==0:
+        print("{} cift bir sayidir.".format(i))
+    elif i%2==1:
+        print("{} tek bir sayidir.".format(i))
---
+++ GirilenSayiKadarEkranaYazdirma(ForDongusu).py
@@ -0,0 +1,5 @@
+sayi=int(input("Lutfen bir sayi giriniz: "))
+yaz=range(0,sayi+1)
+
+for i in yaz:
+    print(i)
